40aa1b48d2011e0df65ec08c51ed84351ae61534
luca288 <luca.mastromatteo06@gmail.com>
2025-03-05 18:08:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function navigate(screenId) {
 function toggleMenu() {
     document.getElementById('sidebar').classList.toggle('open');
 }
+
+function viewDish(dish) {
+    const dishData = {
+        pasta: {
+            title: "Pasta",
+            image: "pasta.jpg",
+            description: "Deliziosa pasta con sugo fresco."
+        },
+        pizza: {
+            title: "Pizza",
+            image: "pizza.jpg",
+            description: "Gustosa pizza con ingredienti freschi."
+        }
+    };
+
+    if (dishData[dish]) {
+        document.getElementById('dish-title').innerText = dishData[dish].title;
+        document.getElementById('dish-image').src = dishData[dish].image;
+        document.getElementById('dish-description').innerText = dishData[dish].description;
+        navigate('dish');
+    }
+}
+
+function addToCart() {
+    alert("Piatto aggiunto al carrello!");
+}
